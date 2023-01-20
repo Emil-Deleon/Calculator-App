a119792796321d38a5efe97a2e97e4c6b0ed64f6
Emil-Deleon <111036019+Emil-Deleon@users.noreply.github.com>
2023-01-20 17:14:14 -0500

    Can add num/operators. AC & Delete working

--- app/src/main/java/com/example/kotlincalculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlincalculatorapp/MainActivity.kt
@@ -3,16 +3,59 @@ package com.example.kotlincalculatorapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
+
+
+    private var canAddOperation = false
+    private var canAddDecimal = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun numAction(view: View) {}
-    fun operatorsAction(view: View) {}
-    fun backspaceAction(view: View) {}
-    fun allClearAction(view: View) {}
-    fun equalsAction(view: View) {}
+    fun numAction(view: View) {
+        if (view is Button) {
+            if (view.text == ".") {
+                if (canAddDecimal) {
+                    workingView.append(view.text)
+                    canAddDecimal = false
+                }
+            }
+            else {
+                workingView.append(view.text)
+            }
+            canAddOperation = true
+        }
+    }
+
+    fun operatorsAction(view: View) {
+        if (view is Button && canAddOperation) {
+            workingView.append(view.text)
+            canAddOperation = false
+            canAddDecimal = true
+        }
+    }
+
+    fun backspaceAction(view: View) {
+        val length = workingView.length()
+        if (length > 0) {
+            workingView.text = workingView.text.subSequence(0, length - 1)
+        }
+    }
+
+    fun allClearAction(view: View) {
+        workingView.text = ""
+        resultView.text = ""
+    }
+
+    fun equalsAction(view: View) {
+        resultView.text = calculateResults()
+    }
+
+
 }
